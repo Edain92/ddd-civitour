719c8fa7493cee9ae9774a79ecc6cb9b2ae1848e
Edäin <mpizarrocuevas@gmail.com>
2023-07-03 20:06:09 +0200

    chore: project structure

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from './index';
-
-describe('Testing jest config', () => {
-  test('Const must have hello value', () => {
-    expect(hello).toBe('HELLO CONFIGS');
-  });
-});
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export const hello = 'HELLO CONFIGS';
