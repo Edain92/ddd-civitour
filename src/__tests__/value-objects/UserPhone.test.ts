3ccb52807a03ee22eadd5a8ecc0807f3b1400293
Edäin <mpizarrocuevas@gmail.com>
2023-07-09 18:00:06 +0200

    feat: userPhone, Guide + testing

---
+++ src/__tests__/value-objects/UserPhone.test.ts
@@ -0,0 +1,20 @@
+import { UserPhone } from "../../value-objects/UserPhone";
+
+describe('ValueObject: UserPhone', () => {
+  const validSpanishPhone = "+34 600000000";
+  const invalidPhone = "Invalid phone number";
+
+  it('Should create a valid UserPhone object for a valid spanish phone number', () => {
+    const result = UserPhone.create(validSpanishPhone);
+    const userPhone = result.getValue();
+
+    expect(result.isSuccess).toBe(true);
+    expect(userPhone.value).toEqual(validSpanishPhone);
+  });
+
+  it('Should fail to create a UserPhone object for a invalid phone number', () => {
+    const result = UserPhone.create(invalidPhone);
+
+    expect(result.isFailure).toBe(true);
+  });
+});
