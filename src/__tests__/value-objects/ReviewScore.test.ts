283ee0a5a44bd711fd113d8dd5591452b78adca3
Edäin <mpizarrocuevas@gmail.com>
2023-07-10 10:10:02 +0200

    fix: var names

--- src/__tests__/value-objects/ReviewScore.test.ts
+++ src/__tests__/value-objects/ReviewScore.test.ts
@@ -3,10 +3,10 @@ import { ReviewScore } from "../../value-objects/ReviewScore";
 describe('ValueObject: ReviewScore', () => {
   it('Should create a valid ReviewScore object', () => {
     const result = ReviewScore.create(4);
-    const tourCapacity = result.getValue();
+    const reviewScore = result.getValue();
 
     expect(result.isSuccess).toBe(true);
-    expect(tourCapacity.value).toEqual(4);
+    expect(reviewScore.value).toEqual(4);
   });
 
   it('Should fail to create a ReviewScore', () => {
--- src/__tests__/value-objects/ReviewTip.test.ts
+++ src/__tests__/value-objects/ReviewTip.test.ts
@@ -3,10 +3,10 @@ import { ReviewTip } from "../../value-objects/ReviewTip";
 describe('ValueObject: ReviewTip', () => {
   it('Should create a valid ReviewTip object', () => {
     const result = ReviewTip.create(50);
-    const tourCapacity = result.getValue();
+    const reviewTip = result.getValue();
 
     expect(result.isSuccess).toBe(true);
-    expect(tourCapacity.value).toEqual(50);
+    expect(reviewTip.value).toEqual(50);
   });
 
   it('Should fail to create a ReviewTip', () => {
